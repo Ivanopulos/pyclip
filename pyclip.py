9b3901e1489fb5e31e70e72d5fb1b0abf6a53c61
Ivanopulos <Pochta.rus.lc@gmail.com>
2023-07-26 12:18:12 +0300

    6

--- pyclip.py
+++ pyclip.py
@@ -1,46 +1,89 @@
 import pyperclip
 import re
+import pandas as pd
 
-m = pyperclip.paste()
-m = re.sub(r"на.?сумму", ' ххх ', m)
-m = re.sub(r"выполнение", ' ххх ', m)
-m = re.sub(r"-", ' ххх ', m)
-m = re.sub(r"ФАП(\w)", r'ФАП \1', m)
-m = re.sub(r"  ", ' ', m)
-n = re.findall(r'ФАП .{3,20} ххх', m)  # n = re.sub(r'w\:fill\=\"(.{4,6})\"', "w:fill=\"AAAAAA\"", m)
-n = set(n) #уборка дублей
-print(n)
-for b in n:
-    print(b)
-print(m)
-
-
-
-
-фыв="удаление лишней формулы из 6700"
+asd = "игра в слова, окончания"
+# m = pyperclip.paste()
+# m = re.sub(r'[ьъы]', r'', m)
+# m = re.sub(r'[й]', r'и', m)
+# #m = re.sub(r'[ё]', r'е', m)
+# # разделение текста на список слов
+# words = m.splitlines()
+# # создаем пустой словарь для подсчета
+# letters_dict = {chr(i): {'start': 0, 'end': 0} for i in range(ord('а'), ord('я')+1)}
+# letters_dict['ё'] = {'start': 0, 'end': 0}
+# # подсчет
+# for word in words:
+#     if word:
+#         letters_dict[word[0]]['start'] += 1
+#         letters_dict[word[-1]]['end'] += 1
+# # создание DataFrame из словаря
+# df = pd.DataFrame.from_dict(letters_dict, orient='index', columns=['start', 'end'])
+# # добавление столбца с отношением
+# df['ratio'] = df['end'] / df['start']
+# df['dif'] = df['end'] - df['start']
+# # сортировка по убыванию 4-го столбца (ratio)
+# df = df.sort_values('dif', ascending=False)
+# # возврат отсортированного DataFrame
+# print(df)
+asd = "подсчет тгсервиса"
+# m = pyperclip.paste()
+# m = m + "\r\n"  # иначе не ест последнее значение
+# print(m)
+# n = re.findall(r'(-?)(\d+)(?=[^\*]*\r)', m)
+# c = 0
+# print(n)
+# for a, b in n:
+#     c = c+int(a+b)
+#     if a == "":
+#         a = "+"
+#     print(a + b)
+# print("Итого: "+str(c)+"/2="+str(c/2)+" к перечислению, высылаю")
+asd = "слова на а"
+# m = pyperclip.paste()
+# n = re.findall(r'^а\w+а:', m, flags=re.MULTILINE)
+# print(n)
+# for b in n:
+#     print(b[:-1])
+# asd = "псеврекурсивное удаление скобок"
+# m = pyperclip.paste()
+# mlen = len(m)
+# mlen1 = 99999999999
+# while mlen < mlen1:
+#     mlen1 = mlen
+#     m = re.sub(r'(ЕСЛИ\([^\(\);]{1,10}\([^)]*?)\([^\(\)]*?\)', r'\1', m)
+#     mlen = len(m)
+# m = re.sub(r'(ЕСЛИ\()[^\(\);]{1,10}\([^)]*?\)', r'\1', m)
+# m = re.sub(r' "";', r'', m)
+# m = re.sub(r'" & ";', r'', m)
+#
+# #            начало                # после базовой скобки
+# #                   до 10 не[();]        # найди финальную внутр скобку
+# print(m) #
+asd = "отковыривание из условий гугла"
+# n = re.findall(r'".*?"', m)
+asd = "удаление лишней формулы из 6700"
 # m = pyperclip.paste()
 # m = re.sub(r"(?<!\))+\n.+\$[BC][A-Z]8(.+\n){3}\s+\.build\(\),", '', m)
 # #m = re.sub(r"SpreadsheetApp\.newConditionalFormatRule\(\)\n.+\$[BC][A-Z]8(.+\n)+\s+\.build\(\),", '', m)
 # pyperclip.copy(m)
 # print(m)
-
-
-фыв="добавление кавычки после формулы, равно и эндэнд для сцепки "#=N7&"Page 1'!A4"&"&"&N7&"Page 1'!B4"&"&"&N7&"Page 1'!D4""
+asd = "добавление кавычки после формулы, равно и эндэнд для сцепки "#=N7&"Page 1'!A4"&"&"&N7&"Page 1'!B4"&"&"&N7&"Page 1'!D4""
 # m = pyperclip.paste()
 # m=re.sub(r"(Page 1'![ABD]\d{1,2})", r'\1"', m)
 # m=re.sub(r"(?<!&)N7", '=N7', m)
 # m=re.sub(r"&N7", '&"&"&N7', m)
 # print(m)
 # pyperclip.copy(m)
-
-фыв="ре серч скобок"
+asd = "ре серч скобок"
 # m = pyperclip.paste()  # коллекция для фигур хтмл
 # a="gfsgdg4gfsdfg54g"
 # b = re.search("\D(\d)\D", a)
 # print(b[1])
-фыв="нумеровалка фигурных скобок"
-# # нумеровалка фигурных скобок
+asd = "нумеровалка фигурных скобок"
+# нумеровалка фигурных скобок
 # bstr = 7412
+# m = pyperclip.paste()
 # n = re.findall(r'\{\}', m)  # n = re.sub(r'w\:fill\=\"(.{4,6})\"', "w:fill=\"AAAAAA\"", m)
 # print(n)
 # for b in n:
@@ -48,12 +91,12 @@ print(m)
 #     bstr += 1
 # pyperclip.copy(m)
 # print(m)
-фыв="склеивание ворда от битых скобок"
+asd = "склеивание ворда от битых скобок"
 # # склеивание ворда битых скобок
 # m = re.sub(r"(<w:t>\{\d*)</w:t>.*?(<w:t>(\d+)</w:t>).*?<w:t>(\d*\}</w:t>)", r"\1\3\4", m)
 # m = re.sub(r"(<w:t>\{\d*)</w:t>.*?<w:t>(\d*\}</w:t>)", r"\1\2", m)
 # pyperclip.copy(m)
-фыв="пересортировка листа файлов на нетекстовую сортировку"
+asd = "пересортировка листа файлов на нетекстовую сортировку"
 # # пересортировка листа
 # a = ['1.png', '10.png', '11+.txt', '12+-.png', '13+20.png', '14+.txt', '15.png', '16.png', '17.png', '18+-.png', '19.png', '2.png', '20.png', '21.png', '22.png', '23.png', '24.png', '25.png', '26.png', '27+-.png', '28.png', '29.png', '3.png', '4+.txt', '5+-.png', '6.png', '7.png', '8+-.png', '8+.txt', '9.png']
 # print(a)
